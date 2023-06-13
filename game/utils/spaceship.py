1b97c5a9494fcbe1f5c314bf82d737478778778c
JavierDiaz05 <javierediazs@gmail.com>
2023-06-12 22:12:06 -0500

    Tarea clase 1
    Hacer que la nave se teletransporte al otro lado si supera uno de los bordes laterales.

--- game/utils/spaceship.py
+++ game/utils/spaceship.py
@@ -22,10 +22,16 @@ class Spaceship(Sprite):
 def move_left(self):
     if self.rect.left > 0:
         self.rect.x -= 10
+    
+    if self.rect.x < 0:
+        self.rect.x = SCREEN_WIDTH
 
 def move_right(self):
     if self.rect.right < SCREEN_WIDTH:
         self.rect.x += 10
+    
+    if self.rect.x > SCREEN_WIDTH:
+        self.rect.x = 0
 
 def move_up(self):
     if self.rect.y > SCREEN_HEIGHT // 2:
